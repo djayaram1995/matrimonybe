827f4d2b593d4a4a1368710bf08973d4587f18a2
djayaram1995 <jayaramdeenu@gmail.com>
2024-09-21 20:25:44 +0530

    Docker add

--- MatrimonyBackend/settings.py
+++ MatrimonyBackend/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,11 +86,11 @@ WSGI_APPLICATION = 'MatrimonyBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'matrimonydb',
-        'USER': 'matrimony',
-        'PASSWORD': 'matrimony',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'), # For local development, use 'localhost' or '127.0.0.1'
+        'PORT': os.environ.get('DATABASE_PORT'), # Default PostgreSQL port is usually '5432'
     }
 }
 
